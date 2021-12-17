701f962d336624814c0c2da6828b24e05b930365
Raúl Wolters <56522208+smups@users.noreply.github.com>
2021-12-17 22:29:46 +0100

    added full sadf file test for text data block type

--- rustronomy/rustronomy_sadf/src/test/db_text_test.rs
+++ rustronomy/rustronomy_sadf/src/test/db_text_test.rs
@@ -24,7 +24,13 @@ use std::{
     io::Write
 };
 
-use crate::data_blocks::text::TextDB;
+use crate::{
+    data_blocks::text::TextDB,
+    header::{
+        Header,
+        header_index_block::HeaderIndexBlock
+    }
+};
 
 use rustronomy_core::data_type_traits::io_utils::{
     EncodeAndConsume,
@@ -46,7 +52,7 @@ fn test_db_text_io() {
 
     //Write the db to a file
     //(1) get the tmp folder
-    let path = env::temp_dir().as_path().join("text_db_text_io.sadf");
+    let path = env::temp_dir().as_path().join("text_db_text_io.dbtxt");
     let mut file = File::create(&path).unwrap();
 
     //(2) write the file
@@ -64,5 +70,35 @@ fn test_db_text_io() {
 
 #[test]
 fn test_db_text_full_sadf_io() {
+    //Generate a text db with the lipsum module
+    let txt = lipsum::lipsum(1000);
+    let (text_db1, len) = TextDB::from_text(txt, 1);
+    
+    //Encode the text_db thing
+    let mut enc = text_db1.to_bytes();
+    
+    //Check that the encoded length equals the len parameter
+    assert!(enc.len() as u64 == len);
 
+    //Generate a header to complete the sadf file
+    let mut header = Header::new();
+    header.add_hib(HeaderIndexBlock {
+        id: 0,
+        ptr: 24, //data starts after hib + header fixed data fields
+        length: enc.len() as u64,
+        data_type: 0x0000
+    });
+
+    //Add the header and the data block to make the full sadf file
+    let mut enc_tot = header.to_bytes();
+    enc_tot.append(&mut enc);
+
+    //Write the db to a file
+    //(1) get the tmp folder
+    let path = env::temp_dir().as_path().join("text_db_text_full_io.sadf");
+    let mut file = File::create(&path).unwrap();
+
+    //(2) write the file
+    file.write(&enc_tot).unwrap();
+    file.flush().unwrap();
 }
