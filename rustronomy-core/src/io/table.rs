e3ae5e892f9af27617ef99c07e80465b3638c0c4
Raúl Wolters <56522208+smups@users.noreply.github.com>
2022-07-04 12:28:33 +0200

    added metadata impl for table

--- rustronomy-core/src/io/table.rs
+++ rustronomy-core/src/io/table.rs
@@ -25,3 +25,66 @@
 //! - `Integer` always a `Vec<i64>`
 //! - `Float` always a `Vec<f64>`
 //! - `Text` always a `Vec<String>`
+//!
+//! Columns can be accessed either through their index or through their name, if
+//! one has been supplied.
+
+use std::{
+  collections::HashMap,
+  fmt::{Debug, Display},
+  str::FromStr,
+};
+
+use super::metadata::{
+  priv_hack::PrivDataContainer, GenericMetaDataTag, MetaDataContainer, MetaDataErr, MetaDataTag,
+};
+
+#[derive(Debug, Clone)]
+pub struct Table {
+  data: Vec<Col>,
+  lookup_tbl: HashMap<String, usize>,
+  meta: HashMap<String, String>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Col {
+  Integer(Vec<i64>),
+  Float(Vec<f64>),
+  Text(String),
+}
+
+impl<T> PrivDataContainer<T> for Table
+where
+  T: Display + Sized + Send + Sync + FromStr,
+  <T as FromStr>::Err: Debug,
+{
+  fn add_priv_tag(&mut self, tag: GenericMetaDataTag<T>) -> Result<(), MetaDataErr> {
+    //(1) Check if the key already exists
+    if self.meta.contains_key(&tag.key) {
+      return Err(MetaDataErr::KeyExists(tag.key));
+    }
+
+    //(2) we're good -> add the key
+    let (key, value) = tag.as_string_pair();
+    self.meta.insert(key, value);
+    Ok(())
+  }
+
+  fn remove_priv_tag(&mut self, key: &str) -> Result<GenericMetaDataTag<T>, MetaDataErr> {
+    //(1) Check if the key does not exists
+    if !self.meta.contains_key(key) {
+      return Err(MetaDataErr::KeyNotFound(key.to_string()));
+    }
+
+    //(2) we're good -> remove the key
+    let (key, value) = self.meta.remove_entry(key).unwrap();
+    Ok(GenericMetaDataTag::<T>::parse_string_pair(key, &value))
+  }
+}
+
+impl<T> MetaDataContainer<T> for Table
+where
+  T: Display + Sized + Send + Sync + FromStr,
+  <T as FromStr>::Err: Debug,
+{
+}
